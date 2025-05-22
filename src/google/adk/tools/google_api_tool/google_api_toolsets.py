fe618c3d930832358dc4de9c93c3291154556972
Wei Sun (Jack) <weisun@google.com>
2025-05-22 11:13:21 -0700

    chore: use relative import in google_api_toolsets.py. Adds open source header for base_toolset.py
    PiperOrigin-RevId: 762042799

--- src/google/adk/tools/google_api_tool/google_api_toolsets.py
+++ src/google/adk/tools/google_api_tool/google_api_toolsets.py
@@ -18,8 +18,7 @@ from typing import List
 from typing import Optional
 from typing import Union
 
-from google.adk.tools.base_toolset import ToolPredicate
-
+from ..base_toolset import ToolPredicate
 from .google_api_toolset import GoogleApiToolset
 
 logger = logging.getLogger("google_adk." + __name__)
