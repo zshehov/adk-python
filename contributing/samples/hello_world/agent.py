e21ea161f5a042127a8ac3766a9c640c023b71e8
Hangfei Lin <hangfei@google.com>
2025-06-03 18:58:31 -0700

    chore: Update agent.py
    Merge https://github.com/google/adk-python/pull/1136
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/adk-python/pull/1136 from google:hangfei-patch-1 63ff81eb2f152cf74a8a610562999101a6362ef8
    PiperOrigin-RevId: 766913586

--- contributing/samples/hello_world/agent.py
+++ contributing/samples/hello_world/agent.py
@@ -68,7 +68,7 @@ async def check_prime(nums: list[int]) -> str:
 
 root_agent = Agent(
     model='gemini-2.0-flash',
-    name='data_processing_agent',
+    name='hello_world_agent',
     description=(
         'hello world agent that can roll a dice of 8 sides and check prime'
         ' numbers.'
